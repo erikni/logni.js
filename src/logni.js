6b372c8dd620181d6fe561b0f21a3c4394149621
Erik Brozek <erik@brozek.name>
2019-08-07 14:03:18 +0200

    Changed: v0.2.4

--- src/logni.js
+++ src/logni.js
@@ -35,7 +35,7 @@
  * logni.error("log error test");
  * </pre>
  *
- * @version 0.2.3
+ * @version 0.2.4
  * @author Erik Brozek - https://github.com/erikni
  * @since 2017
  * @static
@@ -44,7 +44,7 @@
 
 
 // version
-const version = '0.2.3-3';
+const version = '0.2.4';
 
 
 // nodejs compatible mode
